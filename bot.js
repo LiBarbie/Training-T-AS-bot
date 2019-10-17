6858626a9f62a68513396753e3ecaf14b200e7a3
Linda Barbiero <linda.barbiero.st@everis.com>
2019-10-17 11:24:18 +0100

    Added suggested actions/questions as a help for the user

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ActivityHandler } = require('botbuilder');
+const { ActivityHandler, MessageFactory } = require('botbuilder');
 const { QnAMaker } = require('botbuilder-ai');
 
 class MyBot extends ActivityHandler {
@@ -10,11 +10,7 @@ class MyBot extends ActivityHandler {
         if (!configuration) throw new Error('[QnaMakerBot]: Missing parameter. configuration is required');
         // now create a qnaMaker connector.
         this.qnaMaker = new QnAMaker(configuration, qnaOptions);
-
         
-        // Define the main dialog and its related components.
-        this.choiceCardStep.bind(this);
-
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
             // send user input to QnA Maker.
@@ -28,6 +24,7 @@ class MyBot extends ActivityHandler {
                 // If no answers were returned from QnA Maker, reply with help.
                 await context.sendActivity("We couldn't find any answer to your question. Try write it in a different way.");
             }
+            await this.sendSuggestedActions(context);
             await next();
         });
 
@@ -36,76 +33,31 @@ class MyBot extends ActivityHandler {
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
                     await context.sendActivity('Welcome to the Training T&AS FAQ Bot. Ask me anything!');
+                    await this.sendSuggestedActions(context);
                 }
             }
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
-    }
-
+        
     
-    async choiceCardStep(stepContext) {
-        console.log('MainDialog.choiceCardStep');
-
-        // Create the PromptOptions which contain the prompt and re-prompt messages.
-        // PromptOptions also contains the list of choices available to the user.
-        const options = {
-            prompt: "Don't you know what to ask? We suggest you what you could ask!",
-            retryPrompt: 'That was not a valid choice, please select a card or number from 1 to 9.',
-            choices: this.getChoices()
-        };
-
-        // Prompt the user with the configured PromptOptions.
-        return await stepContext.prompt('cardPrompt', options);
     }
 
-    getChoices() {
-        const cardOptions = [
-            {
-                value: 'I am interested in learning more of an specific technology.',
-                synonyms: ['Available training?']
-            },
-            {
-                value: 'Where\'s the latest courses catalogue?',
-                synonyms: ['Latest internal courses catalogue']
-            },
-            {
-                value: 'Where can I find the updated internal courses calendar?',
-                synonyms: ['Internal courses calendar?']
-            },
-            {
-                value: 'I would like to offer a technology training myself to my colleagues.',
-                synonyms: ['Offer training']
-            },
-            {
-                value: 'How can I access SafariBooksOnline platform?',
-                synonyms: ['SafariBooksOnline access']
-            },
-            {
-                value: 'Can I be aware of specific IT events happening around here?',
-                synonyms: ['Being aware of IT events here']
-            },
-            {
-                value: 'I want to get certified in a technology/area of expertise.',
-                synonyms: ['Get certificate of a technology']
-            },
-            {
-                value: 'What to do if I can\'t find the technology I want in the catalogue or calendar',
-                synonyms: ['Can\'t find the technology I\'m interested']
-            },
-            {
-                value: 'Does everis helps economically with any certification I\'m interested in?',
-                synonyms: ['I\'d like to receive an economic help for my certification']
-            },
-            {
-                value : 'Where should I start training?',
-                synonyms : ['Which path can you recommend for me?']
-            }
-        ];
-
-        return cardOptions;
+    async sendSuggestedActions(turnContext) {
+        var reply = MessageFactory.suggestedActions([
+            'Which are the available training resources?',
+             'Where can I find the latest internal courses catalogue?',
+              'Where can I find the updated internal courses calendar?',
+              'I would like to offer a technology training myself to my colleagues.',
+              'How can I access SafariBooksOnline platform?',
+              'What can I do if I can\'t find the technology I want in the catalogue or calendar?',
+              'I want to get certified in a technology/area of expertise.',
+              'Does your company help economically with any certification I\'m interested in?',
+              'Can I be aware of specific IT events happening around here?',
+              'I\'m unsure about which learning path I should start with.'
+            ],"Just ask! If you're not sure about what to ask, you can choose one question here below.");
+        await turnContext.sendActivity(reply);
     }
-
 }
 
 module.exports.MyBot = MyBot;
