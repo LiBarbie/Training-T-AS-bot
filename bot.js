64ca9d85ed551ed3dc926ee34ad2f58e54277f23
Linda Barbiero <linda.barbiero.st@everis.com>
2019-12-05 12:50:32 +0000

    tried 10 elements in carousel cards

--- bot.js
+++ bot.js
@@ -69,10 +69,7 @@ class MyBot extends ActivityHandler {
               'Want to get certified in a technology/area.',
               'Get help to achieve certification by Everis.',
               'Be aware of specific IT events.',
-              'Unsure about the starting learning path.',
-              'Which are the Java certifications?',
-              'Whom can I ask for SQL certifications?',
-              'Avaiable Cobol resources in O\'Reilly?'
+              'Unsure about the starting learning path.'
             ],"Suggested Questions :");
         await turnContext.sendActivity(reply);
     }//sendSuggestedActions
